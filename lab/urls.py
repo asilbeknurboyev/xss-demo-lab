cc9823f78bcf3f6c258c6798a1e78ad4f6a2c882
Asilbek <asilbeknurboyev1706@gmail.com>
2026-04-06 22:58:25 +0500

    DOM XSS labaratoriyasi   qo'shildi

--- lab/urls.py
+++ lab/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lab_view, name='lab_view'), # Bu avvalgi Stored XSS uchun edi
-    path('dom/', views.dom_view, name='dom_view'), # <-- MANA SHU QATORNI QO'SHDIK
+    # Asosiy menyu (Dashboard)
+    path('', views.index, name='index'), 
+    
+    # 3 ta zaiflik manzillari
+    path('reflected/', views.reflected_view, name='reflected_view'),
+    path('stored/', views.lab_view, name='stored_view'), 
+    path('dom/', views.dom_view, name='dom_view'),
 ]
--- lab/views.py
+++ lab/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Izoh
 
-def lab_view(request):
-    # 1. Agar foydalanuvchi "Jo'natish" tugmasini bossa (POST so'rov)
-    if request.method == 'POST':
-        kiritilgan_matn = request.POST.get('xabar') # HTML'dagi input nomi 'xabar' bo'lishi kerak
-        if kiritilgan_matn:
-            # Matnni ma'lumotlar bazasiga saqlaymiz (Stored XSS uchun tayyorgarlik)
-            Izoh.objects.create(matn=kiritilgan_matn)
-        return redirect('/lab/') # Ma'lumot saqlangach, sahifani qayta yuklaymiz
+# 1. BOSH SAHIFA (Asosiy menyu)
+def index(request):
+    return render(request, 'index.html')
 
-    # 2. Bazada saqlangan barcha izohlarni o'qib olamiz
-    barcha_izohlar = Izoh.objects.all().order_by('-vaqt')
+# 2. REFLECTED XSS (Qidiruv tizimidagi zaiflik)
+def reflected_view(request):
+    kiritilgan_matn = request.GET.get('qidiruv', '')
+    return render(request, 'reflected.html', {'matn': kiritilgan_matn})
+
+# 3. STORED XSS (Ma'lumotlar bazasidagi zaiflik)
+def lab_view(request):
+    if request.method == 'POST':
+        kiritilgan_matn = request.POST.get('xabar')
+        if kiritilgan_matn:
+            Izoh.objects.create(matn=kiritilgan_matn)
+        return redirect('/lab/stored/') # Manzil endi stored bo'ldi
     
-    # 3. Ularni HTML sahifaga yuboramiz
-# 3. Ularni HTML sahifaga yuboramiz
+    barcha_izohlar = Izoh.objects.all().order_by('-vaqt')
     return render(request, 'stored.html', {'izohlar': barcha_izohlar})
+
+# 4. DOM-BASED XSS (JavaScript zaifligi)
 def dom_view(request):
     return render(request, 'dom.html')
